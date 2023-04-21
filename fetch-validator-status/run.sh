bda11db60b0e469627a4aae3f9aa8b36ce5916b7
Chinthaka Rajapakshe <101819110+crajapakshe@users.noreply.github.com>
2023-04-21 15:22:12 -0600

    updated path

--- fetch-validator-status/run.sh
+++ fetch-validator-status/run.sh
@@ -29,9 +29,9 @@ else
   export DOCKER_INTERACTIVE="-it"
 fi
 
-docker build -t fetch_status . > /dev/null 2>&1
+/usr/bin/docker build -t fetch_status . > /dev/null 2>&1
 
-cmd="${terminalEmu} docker run --rm ${DOCKER_INTERACTIVE} \
+cmd="${terminalEmu} /usr/bin/docker run --rm ${DOCKER_INTERACTIVE} \
   -e "GENESIS_PATH=${GENESIS_PATH}" \
   -e "GENESIS_URL=${GENESIS_URL}" \
   -e "SEED=${SEED}""
